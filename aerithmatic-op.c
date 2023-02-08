232accaa2a8f7c496b62849e00f916c6df86b008
Kp-patel <121292737+Kp-patel@users.noreply.github.com>
2023-02-08 17:26:30 +0530

    Add files via upload

---
+++ aerithmatic-op.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a, b, ans;
+	printf("\Enter the value of a & b :");
+	scanf("%d %d",&a,&b);
+	ans = a+b;
+	printf("\nAddition is =%d",a+b);
+	ans = a-b;
+	printf("\nSubstraction is =%d",a-b);
+	ans = a*b;
+	printf("\nMultification is =%d",a*b);
+	ans = a/b;
+	printf("\nDivision is =%d",a/b);
+		
+}
